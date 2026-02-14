f3531b4e9582912e72e66437c4012c587b43bfa4
Jumana Sathik <jumanasathik4002@gmail.com>
2026-02-14 23:04:20 +0530

    Calculate protein molecular weight in R
    This script calculates the molecular weight of a protein sequence by summing amino acid weights and adjusting for water loss during peptide bond formation.

---
+++ protein_molecular_weight.R
@@ -0,0 +1,23 @@
+protein <- "MKWVTFISLLFLFSSAYS"
+protein <- toupper(protein)
+
+aa_weights <- c(
+  A=89.09, R=174.20, N=132.12, D=133.10,
+  C=121.15, Q=146.15, E=147.13, G=75.07,
+  H=155.16, I=131.17, L=131.17, K=146.19,
+  M=149.21, F=165.19, P=115.13, S=105.09,
+  T=119.12, W=204.23, Y=181.19, V=117.15
+)
+
+aa_list <- strsplit(protein, "")[[1]]
+
+mw <- sum(aa_weights[aa_list])
+
+length_protein <- nchar(protein)
+
+if (length_protein > 1) {
+  mw <- mw - ((length_protein - 1) * 18.015)
+}
+
+cat("Protein:", protein, "\n")
+cat("Molecular Weight:", round(mw, 2), "Da\n")
